1290dd70c7b100b784eb23ab5384aa316b9ceb9f
antonovkrd <vl.antonov@icloud.com>
2022-03-15 20:15:51 +0300

    initial commit

---
+++ src/main/java/krd/antonov/audiclubsheriff/AudiClubSheriffApplication.java
@@ -0,0 +1,13 @@
+package krd.antonov.audiclubsheriff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiClubSheriffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiClubSheriffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/krd/antonov/audiclubsheriff/AudiClubSheriffApplicationTests.java
@@ -0,0 +1,13 @@
+package krd.antonov.audiclubsheriff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiClubSheriffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
