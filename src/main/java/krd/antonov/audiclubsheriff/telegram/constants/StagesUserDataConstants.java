0fa2fde897f988b5db0266da2b9eaca6450142d4
antonovkrd <vl.antonov@icloud.com>
2022-03-30 19:38:19 +0300

    change StagesUserDataConstants

---
+++ src/main/java/krd/antonov/audiclubsheriff/telegram/constants/StagesUserDataConstants.java
@@ -0,0 +1,10 @@
+package krd.antonov.audiclubsheriff.telegram.constants;
+
+public class StagesUserDataConstants {
+    public static final String USER_PHONE_STAGE = "USER_PHONE";
+    public static final String USER_NAME_STAGE = "USER_NAME";
+    public static final String USER_DATE_BIRTH_STAGE = "USER_DATE_BIRTH";
+    public static final String VEHICLE_LICENCE_PLATE_STAGE = "VEHICLE_LICENCE_PLATE";
+    public static final String VEHICLE_MODEL_STAGE = "VEHICLE_MODEL";
+    public static final String VEHICLE_YEAR_STAGE = "VEHICLE_YEAR";
+}
