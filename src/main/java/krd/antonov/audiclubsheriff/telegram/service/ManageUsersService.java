85fb12c10514ac46d51a6788e8d58c965b80480c
antonovkrd <vl.antonov@icloud.com>
2022-04-05 01:21:07 +0300

    add save user with vehicle

--- src/main/java/krd/antonov/audiclubsheriff/telegram/service/ManageUsersService.java
+++ src/main/java/krd/antonov/audiclubsheriff/telegram/service/ManageUsersService.java
@@ -1,24 +1,48 @@
 package krd.antonov.audiclubsheriff.telegram.service;
 
+import krd.antonov.audiclubsheriff.exceptions.TempDataNotFoundException;
+import krd.antonov.audiclubsheriff.model.TempData;
+import krd.antonov.audiclubsheriff.model.User;
+import krd.antonov.audiclubsheriff.model.Vehicle;
 import krd.antonov.audiclubsheriff.service.TempDataService;
 import krd.antonov.audiclubsheriff.service.UserService;
-import krd.antonov.audiclubsheriff.service.VehicleService;
+import krd.antonov.audiclubsheriff.util.DataConverter;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class ManageUsersService {
 
     private final TempDataService dataService;
     private final UserService userService;
-    private final VehicleService vehicleService;
 
-    public ManageUsersService(TempDataService dataService, UserService userService, VehicleService vehicleService) {
+    public ManageUsersService(TempDataService dataService, UserService userService) {
         this.dataService = dataService;
         this.userService = userService;
-        this.vehicleService = vehicleService;
     }
 
-    public void registerUser(String chatId){
-
+    public void registerUserWithVehicle(String chatId) throws TempDataNotFoundException {
+        List<TempData> tempData = dataService.getListTempDataByChatId(chatId);
+        User user = new User().setDateRegistration(LocalDateTime.now());
+        Vehicle vehicle = new Vehicle();
+        for (TempData data : tempData) {
+            switch (data.getStage()) {
+                case 0 -> user.setTgNickname(data.getValue());
+                case 1 -> user.setPhone(data.getValue());
+                case 2 -> {
+                    user.setName(data.getValue());
+                    user.setChatId(data.getChatId());
+                }
+                case 3 -> user.setDateBirth(DataConverter.convertStringToLocalDate(data.getValue()));
+                case 4 -> user.setCity(data.getValue());
+                case 5 -> vehicle.setModel(data.getValue());
+                case 6 -> vehicle.setYear(Integer.valueOf(data.getValue()));
+                case 7 -> vehicle.setLicensePlate(data.getValue());
+            }
+        }
+        user.addVehicle(vehicle);
+        userService.update(user);
     }
 }
