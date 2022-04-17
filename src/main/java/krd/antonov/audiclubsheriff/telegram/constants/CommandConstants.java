a43ac89cb7b8332aa6cd050ed3dd422106bf4138
antonovkrd <vl.antonov@icloud.com>
2022-04-18 00:22:31 +0300

    new CommandConstants

--- src/main/java/krd/antonov/audiclubsheriff/telegram/constants/CommandConstants.java
+++ src/main/java/krd/antonov/audiclubsheriff/telegram/constants/CommandConstants.java
@@ -4,4 +4,6 @@ public class CommandConstants {
     public static final String START = "/start";
     public static final String REGISTRATION = "Регистрация";
     public static final String EDIT_DATA = "Изменить данные";
+    public static final String ACCEPT = "accept_";
+    public static final String DECLINE = "decline_";
 }
